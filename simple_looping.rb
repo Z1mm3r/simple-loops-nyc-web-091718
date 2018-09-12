45f2adc9eee8529088aff48c4f617bada542a28e
Casey Zimmer <czimmer@ramapo.edu>
2018-09-12 23:32:22 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -7,10 +7,13 @@ def loop_iterator(number_of_times)
   
   loop do 
     puts phrase
-    counter += 1
+    
     if counter == number_of_times
       break
     end
+    
+    counter += 1
+    
   end
   
   
